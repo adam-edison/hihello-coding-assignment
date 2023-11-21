e4314037ad61478d0223f26e8a3dee8c037c3e3d
Adam Edison <adam.edison@gmail.com>
2023-11-21 01:58:52 +0000

    add test case parameterized rig for state machine

--- src/calculation.ts
+++ src/calculation.ts
@@ -1,7 +1,7 @@
 import { CalculatorState } from './types';
 
 export function calculate(state: CalculatorState, expression: string): CalculatorState {
-  state.value = parseInt(expression);
-
-  return state;
+  return {
+    value: parseInt(expression),
+  };
 }
--- test/calculate.test.ts
+++ test/calculate.test.ts
@@ -1,13 +1,26 @@
 import { calculate } from '../src/calculation';
 import { CalculatorState } from '../src/types';
 
+const testCases = [
+  {
+    name: 'clean value single digit will overwrite stored value',
+    state: { value: 0 },
+    expression: '3',
+    expected: { value: 3 },
+  },
+  {
+    name: 'clean value multiple digits will overwrite stored value',
+    state: { value: 0 },
+    expression: '23456',
+    expected: { value: 23456 },
+  },
+];
+
 describe('calculate function', () => {
-  test('clean value will overwrite stored value', () => {
-    const state: CalculatorState = {
-      value: 0,
-    };
-    const expression = '3';
-    const result: CalculatorState = calculate(state, expression);
-    expect(result.value).toEqual(3);
-  });
+  for (const testCase of testCases) {
+    test(`${testCase.name}`, () => {
+      const result: CalculatorState = calculate(testCase.state, testCase.expression);
+      expect(result).toStrictEqual(testCase.expected);
+    });
+  }
 });
