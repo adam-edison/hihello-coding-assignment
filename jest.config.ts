e4c207e1fd35628f5807f9b43a97769899d231a0
Adam Edison <adam.edison@gmail.com>
2023-11-21 01:13:47 +0000

    add eslint, prettier, and jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './',
+  testRegex: '.*\\.test\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  verbose: true,
+  reporters: ['default', 'summary'],
+};
+
+export default config;
